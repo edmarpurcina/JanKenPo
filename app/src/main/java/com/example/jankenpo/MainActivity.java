274325f1f437d94f68a3fc7e2e704d1d48897d26
edmarpurcina <edmar.purcina@gmail.com>
2021-01-13 17:35:46 -0300

    Criação da logica do JanKenPô

--- app/src/main/java/com/example/jankenpo/MainActivity.java
+++ app/src/main/java/com/example/jankenpo/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.jankenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String player_choice){
+
+        System.out.println("Opção Selecionada " + player_choice);
+        int number_random = new Random().nextInt(3);
+        String[] app_choice = {"pedra","papel", "tesoura"};
+        ImageView imageView = findViewById(R.id.ImageResultado);
+        TextView result = findViewById(R.id.textView2);
+
+        switch (app_choice[number_random]){
+            case "pedra":
+                imageView.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imageView.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imageView.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if (app_choice[number_random] == player_choice){ //empate
+            result.setText("Ouve Um empate");
+        }else if( (app_choice[number_random] == "pedra" && player_choice== "tesoura" ) ||
+                (app_choice[number_random] == "tesoura" && player_choice== "papel" ) ||
+                (app_choice[number_random] == "papel" && player_choice== "pedra" ) ){ // maquina ganhou
+
+            result.setText("App venceu e vc Perdeu :(");
+
+        }else{
+            result.setText("Você Venceu :D");
+        }
+
     }
 }
